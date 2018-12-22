22164adbc6e80c999831b96c7f37aa556e258560
yjwysytaf <yjwysytaf@163.com>
2018-12-22 22:20:02 +0800

    这是模拟一辆电动车的代码

---
+++ test/elec_car.py
@@ -0,0 +1,32 @@
+class Car():
+    def _init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' +self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has" + str(self.odometer_reading) + "miles on it.")
+
+    def update_odometer(self,mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("you can't roll back an odometer!")
+
+    def increment_odometer(self,miles):
+        self.odometer_reading += miles
+
+
+
+class ElectricCar(Car):
+    def _init_(self,make,model,year):
+        super()._init_(make,model,year)
+
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
